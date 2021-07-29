2dbd11bddc691d5cb3a34718620b452058584338
Adam Brunner <abrunner@salesforce.com>
2021-07-29 12:34:46 -0400

    Add total-stars attribute to starRatingStatic

--- force-app/main/default/lwc/starRatingContainer/starRatingContainer.js
+++ force-app/main/default/lwc/starRatingContainer/starRatingContainer.js
@@ -2,6 +2,7 @@ import { LightningElement, api } from 'lwc';
 
 export default class StarRatingContainer extends LightningElement {
     @api rating = 0;
+    @api totalStars = 0;
     @api isInteractive = false;
 
     handleRatingClick(event) {
--- force-app/main/default/lwc/starRatingStatic/starRatingStatic.js
+++ force-app/main/default/lwc/starRatingStatic/starRatingStatic.js
@@ -2,10 +2,11 @@ import { LightningElement, api, track} from 'lwc';
 
 export default class StarRating extends LightningElement {
     @api rating;
+    @api totalStars;
     @track stars = new Array();
     connectedCallback() {
 
-        for (let i = 0; i < 5; ++i) {
+        for (let i = 0; i < this.totalStars; ++i) {
             this.stars.push(
                 {
                     Index: i,
